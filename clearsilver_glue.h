24821ad39dc4fdb7bc4742a054abb08f30352238
hisaboh <hisaboh@gmail.com>
2012-06-25 15:43:57 +0900

    Hdfを生成するAPIをとりあえず削除。
    GCによるルートHDFのdealocate問題に対応できないため

--- clearsilver_glue.h
+++ clearsilver_glue.h
@@ -139,15 +139,16 @@ static KMETHOD Hdf_dump(CTX, ksfp_t *sfp _RIX)
 
 //## HDF Hdf.getObj(String name)
 // This method allows you to retrieve the HDF object which represents the HDF subtree ad the named hdfpath.
-static KMETHOD Hdf_getObj(CTX, ksfp_t *sfp _RIX)
-{
-	HDF *hdf = RawPtr_to(HDF *, sfp[0]);
-	const char *name = S_text(sfp[1].s);
-	HDF *retHdf = hdf_get_obj(hdf, name);
-	kHdf *obj = (kHdf*)new_kObject(O_ct(sfp[K_RTNIDX].o), NULL);
-	obj->hdf = retHdf;
-	RETURN_(obj);
-}
+// static KMETHOD Hdf_getObj(CTX, ksfp_t *sfp _RIX)
+// {
+// 	HDF *hdf = RawPtr_to(HDF *, sfp[0]);
+// 	const char *name = S_text(sfp[1].s);
+// 	HDF *retHdf = hdf_get_obj(hdf, name);
+// 	kHdf *obj = (kHdf*)new_kObject(O_ct(sfp[K_RTNIDX].o), NULL);
+// 	obj->hdf = retHdf;
+// 	RETURN_(obj);
+// }
+
 //## String Hdf.objValue()
 // This method retrieves the value of the current HDF node. Here is a sample code snippit:
 //   HDF hdf = new HDF();
@@ -193,18 +194,18 @@ static KMETHOD Hdf_copy(CTX, ksfp_t *sfp _RIX)
 //## Hdf Hdf.getNode(String name)
 // Retrieves the HDF object that is the root of the subtree at hdfpath, create the subtree if it doesn't exist
 // TODO: Java版名称はgetOrCreateObj。Java版に合わせて修正すべきかも。
-static KMETHOD Hdf_getNode(CTX, ksfp_t *sfp _RIX)
-{
-	HDF *hdf = RawPtr_to(HDF *, sfp[0]);
-	const char *name = S_text(sfp[1].s);
-	HDF *retHdf;
-	NEOERR *err;
-	err = hdf_get_node(hdf, name, &retHdf);
-	// TODO: エラー処理
-	kHdf *obj = (kHdf*)new_kObject(O_ct(sfp[K_RTNIDX].o), NULL);
-	obj->hdf = retHdf;
-	RETURN_(obj);
-}
+// static KMETHOD Hdf_getNode(CTX, ksfp_t *sfp _RIX)
+// {
+// 	HDF *hdf = RawPtr_to(HDF *, sfp[0]);
+// 	const char *name = S_text(sfp[1].s);
+// 	HDF *retHdf;
+// 	NEOERR *err;
+// 	err = hdf_get_node(hdf, name, &retHdf);
+// 	// TODO: エラー処理
+// 	kHdf *obj = (kHdf*)new_kObject(O_ct(sfp[K_RTNIDX].o), NULL);
+// 	obj->hdf = retHdf;
+// 	RETURN_(obj);
+// }
 
 //## void readString(String data)
 // parses/loads the contents of the given string as HDF into the current HDF object
@@ -220,49 +221,49 @@ static KMETHOD Hdf_readString(CTX, ksfp_t *sfp _RIX)
 
 //## Hdf Hdf.objChild()
 // This method is used to walk the HDF tree. Keep in mind that every node in the tree can have a value, a child, and a next peer.
-static KMETHOD Hdf_objChild(CTX, ksfp_t *sfp _RIX)
-{
-	HDF *hdf = RawPtr_to(HDF *, sfp[0]);
-	HDF *retHdf = hdf_obj_child(hdf);
-	kHdf *obj = (kHdf*)new_kObject(O_ct(sfp[K_RTNIDX].o), NULL);
-	obj->hdf = retHdf;
-	RETURN_(obj);
-}
+// static KMETHOD Hdf_objChild(CTX, ksfp_t *sfp _RIX)
+// {
+// 	HDF *hdf = RawPtr_to(HDF *, sfp[0]);
+// 	HDF *retHdf = hdf_obj_child(hdf);
+// 	kHdf *obj = (kHdf*)new_kObject(O_ct(sfp[K_RTNIDX].o), NULL);
+// 	obj->hdf = retHdf;
+// 	RETURN_(obj);
+// }
 
 //## Hdf Hdf.getChild(String name)
 // Retrieves the HDF for the first child of the root of the subtree at hdfpath, or null if no child exists of that path or if the path doesn't exist.
-static KMETHOD Hdf_getChild(CTX, ksfp_t *sfp _RIX)
-{
-	HDF *hdf = RawPtr_to(HDF *, sfp[0]);
-	const char *name = S_text(sfp[1].s);
-	HDF *retHdf = hdf_get_child(hdf, name);
-	kHdf *obj = (kHdf*)new_kObject(O_ct(sfp[K_RTNIDX].o), NULL);
-	obj->hdf = retHdf;
-	RETURN_(obj);
-}
+// static KMETHOD Hdf_getChild(CTX, ksfp_t *sfp _RIX)
+// {
+// 	HDF *hdf = RawPtr_to(HDF *, sfp[0]);
+// 	const char *name = S_text(sfp[1].s);
+// 	HDF *retHdf = hdf_get_child(hdf, name);
+// 	kHdf *obj = (kHdf*)new_kObject(O_ct(sfp[K_RTNIDX].o), NULL);
+// 	obj->hdf = retHdf;
+// 	RETURN_(obj);
+// }
 
 //## Hdf Hdf.objTop()
 // Return the root of the tree that this node is in.
 // TODO: Java版の名前はgetRootObj()。Java版に合わせて修正すべきかも。
-static KMETHOD Hdf_objTop(CTX, ksfp_t *sfp _RIX)
-{
-	HDF *hdf = RawPtr_to(HDF *, sfp[0]);
-	HDF *retHdf = hdf_obj_top(hdf);
-	kHdf *obj = (kHdf*)new_kObject(O_ct(sfp[K_RTNIDX].o), NULL);
-	obj->hdf = retHdf;
-	RETURN_(obj);
-}
+// static KMETHOD Hdf_objTop(CTX, ksfp_t *sfp _RIX)
+// {
+// 	HDF *hdf = RawPtr_to(HDF *, sfp[0]);
+// 	HDF *retHdf = hdf_obj_top(hdf);
+// 	kHdf *obj = (kHdf*)new_kObject(O_ct(sfp[K_RTNIDX].o), NULL);
+// 	obj->hdf = retHdf;
+// 	RETURN_(obj);
+// }
 
 //## Hdf Hdf.objNext()
 // This method is used to walk the HDF tree to the next peer.
-static KMETHOD Hdf_objNext(CTX, ksfp_t *sfp _RIX)
-{
-	HDF *hdf = RawPtr_to(HDF *, sfp[0]);
-	HDF *retHdf = hdf_obj_next(hdf);
-	kHdf *obj = (kHdf*)new_kObject(O_ct(sfp[K_RTNIDX].o), NULL);
-	obj->hdf = retHdf;
-	RETURN_(obj);
-}
+// static KMETHOD Hdf_objNext(CTX, ksfp_t *sfp _RIX)
+// {
+// 	HDF *hdf = RawPtr_to(HDF *, sfp[0]);
+// 	HDF *retHdf = hdf_obj_next(hdf);
+// 	kHdf *obj = (kHdf*)new_kObject(O_ct(sfp[K_RTNIDX].o), NULL);
+// 	obj->hdf = retHdf;
+// 	RETURN_(obj);
+// }
 
 //## String objName()
 // This method retrieves the name of the current HDF node. The name only includes the current level. Here is a sample code snippit:
@@ -357,17 +358,17 @@ static kbool_t clearsilver_initPackage(CTX, kKonohaSpace *ks, int argc, const ch
 		_Public, _F(Hdf_writeString), TY_String	, TY_Hdf, MN_("writeString"), 0, 
 		_Public, _F(Hdf_readString)	, TY_void	, TY_Hdf, MN_("readString")	, 1, TY_String, FN_("data"),
 		_Public, _F(Hdf_dump)		, TY_void	, TY_Hdf, MN_("dump")		, 1, TY_String, FN_("prefix"),
-		_Public, _F(Hdf_getObj)		, TY_Hdf	, TY_Hdf, MN_("getObj")		, 1, TY_String, FN_("name"),
+//		_Public, _F(Hdf_getObj)		, TY_Hdf	, TY_Hdf, MN_("getObj")		, 1, TY_String, FN_("name"),
 		_Public, _F(Hdf_objValue)	, TY_String	, TY_Hdf, MN_("objValue")	, 0, 
 		_Public, _F(Hdf_objName)	, TY_String	, TY_Hdf, MN_("objName")	, 0, 
 		_Public, _F(Hdf_getIntValue), TY_Int	, TY_Hdf, MN_("getIntValue"), 2, TY_String, FN_("name"), TY_Int, FN_("defaultValue"),
 		_Public, _F(Hdf_copy)		, TY_void	, TY_Hdf, MN_("copy")		, 2, TY_String, FN_("name"), TY_Hdf, FN_("src"),
 		_Public, _F(Hdf_setCopy)	, TY_void	, TY_Hdf, MN_("setCopy")	, 2, TY_String, FN_("name"), TY_String, FN_("srcName"),
-		_Public, _F(Hdf_getNode)	, TY_Hdf	, TY_Hdf, MN_("getNode")	, 1, TY_String, FN_("name"),
-		_Public, _F(Hdf_objChild)   , TY_Hdf 	, TY_Hdf, MN_("objChild")	, 0,
-		_Public, _F(Hdf_getChild)   , TY_Hdf 	, TY_Hdf, MN_("getChild")	, 1, TY_String, FN_("name"),
-		_Public, _F(Hdf_objTop)   	, TY_Hdf 	, TY_Hdf, MN_("objTop")		, 0,
-		_Public, _F(Hdf_objNext)   	, TY_Hdf 	, TY_Hdf, MN_("objNext")	, 0,
+		// _Public, _F(Hdf_getNode)	, TY_Hdf	, TY_Hdf, MN_("getNode")	, 1, TY_String, FN_("name"),
+		// _Public, _F(Hdf_objChild)   , TY_Hdf 	, TY_Hdf, MN_("objChild")	, 0,
+		// _Public, _F(Hdf_getChild)   , TY_Hdf 	, TY_Hdf, MN_("getChild")	, 1, TY_String, FN_("name"),
+		// _Public, _F(Hdf_objTop)   	, TY_Hdf 	, TY_Hdf, MN_("objTop")		, 0,
+		// _Public, _F(Hdf_objNext)   	, TY_Hdf 	, TY_Hdf, MN_("objNext")	, 0,
 		_Public, _F(Hdf_removeTree)	, TY_void	, TY_Hdf, MN_("removeTree")	, 1, TY_String, FN_("name"),
 		DEND,
 	};
